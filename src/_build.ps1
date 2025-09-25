ccea7de6e4ebdb48664c6e28d752d29772d22e52
Fabien Molinet <molinetf@medgate.ch>
2025-09-25 15:08:52 +0200

    fix

--- src/_build.ps1
+++ src/_build.ps1
@@ -11,9 +11,9 @@ function CleanUp
 
 function CompileForSpecificRoslynVersion([string] $RoslynVersion) {
     Write-Host "Compiling 'SyntaxInspectors.Analyzers' for roslyn$RoslynVersion" -ForegroundColor Cyan
-    dotnet.exe build SyntaxInspectors.Analyzers/SyntaxInspectors.Analyzers.csproj --configuration Release /p:RoslynVersion=roslyn$RoslynVersion
+    dotnet.exe build SyntaxInspectors.Analyzers/SyntaxInspectors.Analyzers.csproj --configuration Release /p:RoslynVersion=roslyn$RoslynVersion /p:TargetFramework=netstandard2.0
     Write-Host "Compiling 'SyntaxInspectors.Analyzers.CodeFixers' for roslyn$RoslynVersion" -ForegroundColor Cyan
-    dotnet.exe build SyntaxInspectors.Analyzers.CodeFixers/SyntaxInspectors.Analyzers.CodeFixers.csproj --configuration Release /p:RoslynVersion=roslyn$RoslynVersion
+    dotnet.exe build SyntaxInspectors.Analyzers.CodeFixers/SyntaxInspectors.Analyzers.CodeFixers.csproj --configuration Release /p:RoslynVersion=roslyn$RoslynVersion /p:TargetFramework=netstandard2.0
 }
 
 function CompileAll {
@@ -26,8 +26,8 @@ function CompileAll {
     CompileForSpecificRoslynVersion "4.13"
     CompileForSpecificRoslynVersion "4.14"
 
-    dotnet restore SyntaxInspectors.Analyzers.Pack/SyntaxInspectors.Analyzers.Pack.csproj
-    dotnet pack SyntaxInspectors.Analyzers.Pack/SyntaxInspectors.Analyzers.Pack.csproj --configuration Release --no-build 
+    dotnet restore SyntaxInspectors.Analyzers.Pack/SyntaxInspectors.Analyzers.Pack.csproj -p:TargetFramework=netstandard2.0
+    dotnet pack SyntaxInspectors.Analyzers.Pack/SyntaxInspectors.Analyzers.Pack.csproj --configuration Release --no-build -p:TargetFramework=netstandard2.0
 }
 
 Push-Location $PSScriptRoot
